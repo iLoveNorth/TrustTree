e90e4185a6c3531431f888980f7f07795b561240
Ilya Labarevich <3868704@gmail.com>
2018-06-09 19:50:15 +0300

    repair

--- TrustTree/SecondViewController.swift
+++ TrustTree/SecondViewController.swift
@@ -12,7 +12,6 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        hello world! 
         // Do any additional setup after loading the view, typically from a nib.
     }
 
